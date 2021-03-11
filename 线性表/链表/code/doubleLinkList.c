6a5b2b19474b1cfee5778d428c2126fecc227b13
doFighter <1582725691@qq.com>
2021-03-11 18:35:14 +0800

    更新双链表内容

--- 线性表/链表/code/doubleLinkList.c
+++ 线性表/链表/code/doubleLinkList.c
@@ -7,11 +7,13 @@ typedef int ElemType;
  *  链表节点的结构体
  * 
  *  - ElemType elem 数据域
- *  - struct Link *next 指针域，指向直接后的节点
+ *  - struct Link *next 指针域，指向直接后继的节点
+ *  - struct Link *prior 指针域，指向直接前驱的节点
  */
 typedef struct Link
 {
     ElemType elem;
+    struct Link *prior;
     struct Link *next;
 } link;
 
@@ -25,6 +27,7 @@ link *initLinkNode(ElemType elem)
     link *node = (link *)malloc(sizeof(link));
     node->elem = elem;
     node->next = NULL;
+    node->prior = NULL;
     return node;
 }
 
@@ -37,12 +40,13 @@ link *initLinkNode(ElemType elem)
 link *initLink(ElemType data[], int length)
 {
     link *start, *end;
-    start = initLinkNode(data[0]);
+    start = initLinkNode(0); // 创建带有空头结点的双链表
     end = start;
-    for (int i = 1; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
         link *node = initLinkNode(data[i]);
         end->next = node;
+        node->prior = end;
         end = node;
     }
     return start;
@@ -69,8 +73,63 @@ void insertLink(link *head, ElemType elem, int site)
         temp = temp->next;
     }
     link *node = initLinkNode(elem);
-    node->next = temp->next;
-    temp->next = node;
+    if (temp->next == NULL)
+    {
+        temp->next = node;
+        node->prior = temp;
+    }
+    else
+    {
+        node->next = temp->next;
+        temp->next->prior = node;
+        node->prior = temp;
+        temp->next = node;
+    }
+}
+
+/**
+ *  插入函数:这个函数是为了方便一下插入大量数据，所以可以接受数组插入
+ *  - link *L   要插入的链表
+ *  - ElemType data[] 要插入元素组成的数组
+ *  - int length    数组的长度
+ *  - int site  要插入的位置
+ *  注意：链表带有一个空的头指针
+ */
+void insertArrayToLink(link *head, ElemType data[], int length, int site)
+{
+    link *temp;
+    temp = head;
+    for (int i = 0; i < site - 1; i++)
+    {
+        if (temp == NULL)
+        {
+            printf("插入位置错误，请重新插入！\n");
+            return;
+        }
+        temp = temp->next;
+    }
+    if (temp->next == NULL)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            link *node = initLinkNode(data[i]);
+            temp->next = node;
+            node->prior = temp;
+            temp = node;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < length; i++)
+        {
+            link *node = initLinkNode(data[i]);
+            node->next = temp->next;
+            temp->next->prior = node;
+            node->prior = temp;
+            temp->next = node;
+            temp = node;
+        }
+    }
 }
 
 /**
@@ -87,13 +146,21 @@ void deleteLink(link *head, int site)
     {
         if (temp == NULL)
         {
-            printf("插入位置错误，请重新插入！\n");
+            printf("删除位置错误，请重新输入！\n");
             return;
         }
         temp = temp->next;
     }
     del = temp->next;
-    temp->next = temp->next->next;
+    if (del->next == NULL)
+    {
+        temp->next = temp->next->next;
+    }
+    else
+    {
+        temp->next = temp->next->next;
+        temp->next->prior = temp;
+    }
     free(del);
 }
 
@@ -112,6 +179,7 @@ int selectLink(link *head, ElemType elem)
     {
         if (temp->elem == elem)
         {
+            printf("%d 元素的位置为 %d,他的是直接前驱元素为：%d,位置为 %d\n", elem, i, temp->prior->elem, i - 1);
             return i;
         }
         temp = temp->next;
@@ -135,7 +203,7 @@ void amendLink(link *head, ElemType newElem, int site)
     {
         if (temp == NULL)
         {
-            printf("插入位置错误，请重新插入！\n");
+            printf("输入的修改位置错误，请重新输入！\n");
             return;
         }
         temp = temp->next;
@@ -166,15 +234,18 @@ int main()
     head = initLinkNode(0);
     int data[] = {11, 22, 33, 44, 55, 66, 77};
     int length = 7;
-    head->next = initLink(data, length);
+    head = initLink(data, length);
     displayLink(head);
     insertLink(head, 100, 4);
     displayLink(head);
     deleteLink(head, 3);
     displayLink(head);
+    int data2[] = {10, 20, 30};
+    insertArrayToLink(head, data2, 3, 8);
+    displayLink(head);
     amendLink(head, 101, 3);
     displayLink(head);
-    int i = selectLink(head, 220);
+    int i = selectLink(head, 101);
     if (i < 0)
     {
         printf("未找到元素\n");
